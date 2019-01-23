47976807a6304c5218b123a39fe16d5d9930ea2a
Tsimofei Sikorski <sikorskitimofei@gmail.com>
2019-01-23 15:01:58 +0300

    add mainwindow.h

---
+++ mainwindow.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <QMainWindow>
+
+#include "ToDoAreaWidgets/TaskAreaWidget/TomorrowTaskAreaWidget/tomorrowtaskareawidget.h"
+#include "ToDoAreaWidgets/TaskAreaWidget/SomeDayTaskAreaWidget/somedaytaskareawidget.h"
+#include "ToDoAreaWidgets/IdeaAreaWidget/ideaareawidget.h"
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow();
+    ~MainWindow();
+
+private slots:
+    void on_doTodayButton_clicked();
+
+    void on_doTomorrowButton_clicked();
+
+    void on_doSomeDayButton_clicked();
+
+    void on_ideasButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    TaskAreaWidget * taskArea;
+    TomorrowTaskAreaWidget * tomorrowTaskArea;
+    SomeDayTaskAreaWidget * someDayTaskArea;
+    IdeaAreaWidget * ideaArea;
+};
+
+
